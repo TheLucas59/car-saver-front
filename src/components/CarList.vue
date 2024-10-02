19254eb5133931b3e94b1bbef071b140ff3323ea
unknown <ple.lucas@yahoo.fr>
2024-10-02 17:22:00 +0200

    added first component to fetch and display cars in a table

---
+++ src/components/CarList.vue
@@ -0,0 +1,41 @@
+<template>
+  <table>
+    <thead>
+        <tr>
+            <th>Voiture</th>
+            <th>Marque</th>
+            <th>Modèle</th>
+        </tr>
+    </thead>
+    <tr v-for="c in cars">
+        <td>Placeholder</td>
+        <td>{{ c.brand }}</td>
+        <td>{{ c.model }}</td>
+    </tr>
+  </table>
+</template>
+
+<script>
+import CarDetails from './CarDetails.vue';
+import baseUrl from '@/api/carSaverApi';
+
+export default {
+    name: 'CarList',
+    components: { CarDetails },
+    data() {
+        return { 
+            cars: []
+        }
+    },
+    created() {
+        this.axios.get(`${baseUrl}/getAll`)
+        .then(response => {
+            this.cars = response.data
+        })
+    }
+}
+</script>
+
+<style>
+
+</style>
